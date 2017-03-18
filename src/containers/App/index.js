5f1bb00ff4d3055f5382387d631fb015dc3325a2
Krit <durunvo1@gmail.com>
2017-03-18 11:10:19 +0700

    include layout and locale provider

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -4,15 +4,46 @@ import style2 from './style.css'
 import { Col, Row } from 'react-styled-flexboxgrid'
 import { DatePicker } from 'antd';
 import Profile from '../../assets/images/KD-Profile.png'
+import { LocaleProvider, Layout, Menu, Breadcrumb } from 'antd';
+const { Header, Content, Footer } = Layout;
+import enUS from 'antd/lib/locale-provider/en_US';
 
 export default () => {
   return (
-    <Row center="xs" className={style.test}>
-      <img src={Profile} />
-      <Col xs={12} md={4}>test1</Col>
-      <Col xs={12} md={4} className={style2.test2}>test2</Col>
-      <Col xs={12} md={4}>test3</Col>
-      <Col xs={12} md={4}><DatePicker /></Col>
-    </Row>
+    <LocaleProvider locale={enUS}>
+      <Layout>
+        <Header>
+          <div className={style2.logo} />
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['2']}
+            style={{ lineHeight: '64px' }} >
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <Breadcrumb style={{ margin: '12px 0' }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+            <Row center="xs" className={style.test}>
+              <Col xs={12} md={4}><img src={Profile} /></Col>
+              <Col xs={12} md={4}>test1</Col>
+              <Col xs={12} md={4} className={style2.test2}>test2</Col>
+              <Col xs={12} md={4}>test3</Col>
+              <Col xs={12} md={4}><DatePicker /></Col>
+            </Row>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          Ant Design ©2016 Created by Ant UED
+        </Footer>
+      </Layout>
+    </LocaleProvider>
   )
 }
